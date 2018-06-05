107b9624955cd3c209aba5c7c46297205ad1d375
iCode18 <alavi.afshin@gmail.com>
2018-06-05 08:49:13 +0200

    every functionality works but with no design

---
+++ ToDoList/assets/js/todos.js
@@ -0,0 +1,25 @@
+// toggle check by clicking
+$("ul").on("click", "li" ,function(){
+	$(this).toggleClass("checked");
+})
+
+//Delete icon remove the blong item
+$("ul").on("click", "span", function(){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	})
+})
+
+//Add new item
+//listen to the input and action on enter key
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13) {
+		//grab the new item
+		var newItem = $(this).val();
+		$(this).val("");
+		//add the new item to list
+		$("ul").append("<li><span>X</span> " + newItem + "</li>")
+	}
+})
+
+
